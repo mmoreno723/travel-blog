98975a47a571838da936301245859d7156f9b4df
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-12-12 20:59:36 +0100

    added bootstrap to html and created boxes for blog posts on the blog page

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,9 +1,51 @@
 import React from "react";
+import "../../style.css";
+
+const boxContainer = {
+  // border: "black 2px solid",
+  paddingTop: "50px",
+};
+
+const box = {
+  border: "blue solid 2px",
+  height: "400px",
+  width: "800px",
+};
 
 export default function Blog() {
   return (
     <>
-      <p>Blog page</p>
+      <div id="boxContainer" class="container" style={boxContainer}>
+        <div class="row">
+          <div id="box" class="col-4" style={box}>
+            <p>box 1</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 2</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 3</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 4</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 5</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 6</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 7</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 8</p>
+          </div>
+          <div id="box" class="col-4" style={box}>
+            <p>box 9</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
