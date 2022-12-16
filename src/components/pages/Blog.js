cc507ea8293549e8d087ca6700f78ac8f8602096
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-12-16 18:47:41 +0100

    boxes for indivudual blog posts spaced correctly with bootstrap

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -2,46 +2,50 @@ import React from "react";
 import "../../style.css";
 
 const boxContainer = {
-  // border: "black 2px solid",
   paddingTop: "50px",
 };
 
 const box = {
-  border: "blue solid 2px",
+  border: "black solid 2px",
   height: "400px",
-  width: "800px",
+};
+
+const boxesGap = {
+  gap: "20px",
 };
 
 export default function Blog() {
   return (
     <>
-      <div id="boxContainer" class="container" style={boxContainer}>
-        <div class="row">
-          <div id="box" class="col-4" style={box}>
+      <div class="container" style={boxContainer}>
+        <div id="boxes" class="row" style={boxesGap}>
+          <div id="box" class="col" style={box}>
             <p>box 1</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div id="box" class="col" style={box}>
             <p>box 2</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div id="box" class="col" style={box}>
             <p>box 3</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div class="w-100"></div>
+          <div id="box" class="col" style={box}>
             <p>box 4</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div id="box" class="col" style={box}>
             <p>box 5</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div id="box" class="col" style={box}>
             <p>box 6</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div class="w-100"></div>
+          <div id="box" class="col" style={box}>
             <p>box 7</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div id="box" class="col" style={box}>
             <p>box 8</p>
           </div>
-          <div id="box" class="col-4" style={box}>
+          <div id="box" class="col" style={box}>
             <p>box 9</p>
           </div>
         </div>
